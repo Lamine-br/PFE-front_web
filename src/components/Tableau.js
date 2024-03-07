b3101e66dfe3a7f9c2ed38292ad365ba0ff9cac7
Lamine-br <jl_brahami@esi.dz>
2024-03-07 14:53:29 +0100

    condition ajouté dans le tableau des offres si data est vide

--- src/components/Tableau.js
+++ src/components/Tableau.js
@@ -11,7 +11,7 @@ import { Popup } from "./Popup";
 import { ButtonCarre } from "./ButtonCarre";
 
 export function Tableau({ data, type, onRowClick }) {
-	const headers = Object.keys(data[0]);
+	const headers = data && data.length > 0 ? Object.keys(data[0]) : [];
 
 	const [numCols, setNumCols] = useState(headers.length + 1);
 
@@ -20,82 +20,97 @@ export function Tableau({ data, type, onRowClick }) {
 
 	return (
 		<div className='w-full mt-6 space-y-1'>
-			<div
-				className={`grid grid-cols-${numCols} text-center bg-bleuF items-center p-2 rounded-lg`}
-			>
-				{headers.map((header, index) => (
-					<p key={index} className='text-violet text-sm font-bold'>
-						{header}
-					</p>
-				))}
-				<p className='text-violet text-sm font-bold'>Actions</p>
-			</div>
-			<div className='w-full space-y-1'>
-				{data.map((item, itemIndex) => (
+			{data.length === 0 ? (
+				<p className='text-bleuF text-lg font-bold'>Pas d'offre disponible</p>
+			) : (
+				<>
 					<div
-						key={itemIndex}
-						className={`grid grid-cols-${numCols} text-center justify-center bg-violet items-center p-2 rounded-lg cursor-pointer`}
-						onClick={() => onRowClick(item.Id)}
+						className={`grid grid-cols-${numCols} text-center bg-bleuF items-center p-2 rounded-lg`}
 					>
 						{headers.map((header, index) => (
-							<p key={index} className='text-bleuF text-sm font-semibold'>
-								{item[header]}
+							<p key={index} className='text-violet text-sm font-bold'>
+								{header}
 							</p>
 						))}
-						<div className='flex justify-center items-center space-x-4'>
-							{type === "offres" ? (
-								<>
-									<FaFolder
-										size={14}
-										color={"#465475"}
-										className='cursor-pointer'
-									/>
-									<FaPen
-										size={12}
-										color={"#465475"}
-										className='cursor-pointer'
-									/>
-									<FaTrash
-										size={12}
-										className='cursor-pointer'
-										color='#FF584D'
-										onClick={(e) => {
-											e.stopPropagation();
-											setShowDeleteConfirmation(true);
-										}}
-									/>
-								</>
-							) : (
-								<>
-									<FaTag size={12} color='#465475' className='cursor-pointer' />
-									<FaCheck
-										size={12}
-										color={
-											item["Statut"] === "En attente" ? "#30CA3F" : "#CCCCCC"
-										}
-										className='cursor-pointer'
-									/>
-									<FaTimes
-										size={14}
-										color={
-											item["Statut"] === "En attente" ? "#FF584D" : "#CCCCCC"
-										}
-										className='cursor-pointer'
-										onClick={(e) => {
-											e.stopPropagation();
-											setShowRefusConfirmation(true);
-										}}
-									/>
-								</>
-							)}
-						</div>
+						<p className='text-violet text-sm font-bold'>Actions</p>
 					</div>
-				))}
-			</div>
+					<div className='w-full space-y-1'>
+						{data.map((item, itemIndex) => (
+							<div
+								key={itemIndex}
+								className={`grid grid-cols-${numCols} text-center justify-center bg-violet items-center p-2 rounded-lg cursor-pointer`}
+								onClick={() => onRowClick(item.Id)}
+							>
+								{headers.map((header, index) => (
+									<p key={index} className='text-bleuF text-sm font-semibold'>
+										{item[header]}
+									</p>
+								))}
+								<div className='flex justify-center items-center space-x-4'>
+									{type === "offres" ? (
+										<>
+											<FaFolder
+												size={14}
+												color={"#465475"}
+												className='cursor-pointer'
+											/>
+											<FaPen
+												size={12}
+												color={"#465475"}
+												className='cursor-pointer'
+											/>
+											<FaTrash
+												size={12}
+												className='cursor-pointer'
+												color='#FF584D'
+												onClick={(e) => {
+													e.stopPropagation();
+													setShowDeleteConfirmation(true);
+												}}
+											/>
+										</>
+									) : (
+										<>
+											<FaTag
+												size={12}
+												color='#465475'
+												className='cursor-pointer'
+											/>
+											<FaCheck
+												size={12}
+												color={
+													item["Statut"] === "En attente"
+														? "#30CA3F"
+														: "#CCCCCC"
+												}
+												className='cursor-pointer'
+											/>
+											<FaTimes
+												size={14}
+												color={
+													item["Statut"] === "En attente"
+														? "#FF584D"
+														: "#CCCCCC"
+												}
+												className='cursor-pointer'
+												onClick={(e) => {
+													e.stopPropagation();
+													setShowRefusConfirmation(true);
+												}}
+											/>
+										</>
+									)}
+								</div>
+							</div>
+						))}
+					</div>
+				</>
+			)}
+
 			{showDeleteConfirmation && (
 				<Popup
 					Titre={"Confirmation"}
-					Texte={"Êtes-vous sur de vouloir supprimer cette offre ?"}
+					Texte={"Êtes-vous sûr de vouloir supprimer cette offre ?"}
 					onConfirm={() => {}}
 					onDismiss={() => setShowDeleteConfirmation(false)}
 				/>
@@ -104,7 +119,7 @@ export function Tableau({ data, type, onRowClick }) {
 			{showRefusConfirmation && (
 				<Popup
 					Titre={"Confirmation"}
-					Texte={"Êtes-vous sur de vouloir refuser cette candidature ?"}
+					Texte={"Êtes-vous sûr de vouloir refuser cette candidature ?"}
 					onConfirm={() => setShowRefusConfirmation(false)}
 					onDismiss={() => setShowRefusConfirmation(false)}
 				/>
