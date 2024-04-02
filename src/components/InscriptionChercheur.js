4973beec68bb58c1e20d4cad89dc47dab99291be
Lamine-br <jl_brahami@esi.dz>
2024-04-02 02:09:43 +0100

    upload image added to inscription chercheur et intégration faite

--- src/components/InscriptionChercheur.js
+++ src/components/InscriptionChercheur.js
@@ -1,63 +1,176 @@
-import React from "react";
+import React, { useState } from "react";
 import { ButtonRond } from "./ButtonRond";
 import { FaFileUpload } from "react-icons/fa";
+import { axiosInstance } from "../util/axios";
 
 export function InscriptionChercheur() {
+	const [formData, setFormData] = useState({
+		email: "",
+		password: "",
+		image: "",
+		nom: "",
+		prenom: "",
+		date_naissance: "",
+		nationalite: "",
+		numero: "",
+		ville: "",
+		cv: "Hi",
+	});
+
+	function handleInputChange(event, field) {
+		const value = event.target.value;
+		setFormData((prevFormData) => ({
+			...prevFormData,
+			[field]: value,
+		}));
+	}
+
+	const [selectedFile, setSelectedFile] = useState(null);
+	const [previewUrl, setPreviewUrl] = useState(null);
+
+	const handleFileChange = (event) => {
+		const file = event.target.files[0];
+		setSelectedFile(file);
+		console.log(file);
+		if (file) {
+			const reader = new FileReader();
+			reader.onloadend = () => {
+				setPreviewUrl(reader.result);
+			};
+			reader.readAsDataURL(file);
+		} else {
+			setPreviewUrl(null);
+		}
+	};
+
+	const handleUpload = async () => {
+		if (selectedFile) {
+			const formData = new FormData();
+			formData.append("image", selectedFile);
+
+			try {
+				const response = await axiosInstance.post("/auth/upload", formData, {
+					headers: {
+						"Content-Type": "multipart/form-data",
+					},
+				});
+				if (response.request.status === 200) {
+					setFormData((prevFormData) => ({
+						...prevFormData,
+						["image"]: response.data,
+					}));
+				}
+			} catch (error) {
+				console.error("Error uploading file:", error);
+			}
+		} else {
+			console.log("Please select a file.");
+		}
+	};
+
+	async function submitData() {
+		try {
+			const response = await axiosInstance.post(
+				`/auth/register/chercheur`,
+				formData
+			);
+
+			if (response.request.status === 200) {
+				console.log(response.data);
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	}
+
+	const handleSubmit = async () => {
+		await handleUpload();
+		console.log(formData);
+		submitData();
+	};
+
 	return (
-		<div className='overlay flex justify-center'>
+		<div className='overlay flex justify-center w-full'>
 			<div className='z-50 justify-center items-center p-4 w-3/4 h-4/5 bg-bleuF rounded-lg'>
 				<h1 className='text-xl text-violet font-bold mb-6 ml-4'>
 					S'inscrire - Chercheur d'emplois
 				</h1>
 
-				<div className='grid grid-cols-4 gap-8 mx-4 mb-10'>
+				<div className='grid grid-cols-3 gap-8 mx-4 mb-10'>
 					<div className='flex flex-col'>
-						<label className='text-violet text-xs font-bold'>Nom</label>
+						<label className='text-violet text-xs font-bold'>
+							Email <span className='text-rouge'>*</span>
+						</label>
 						<input
 							className='bg-violet border border-gray-400 rounded-md p-1 focus:outline-none focus:border-blue-500'
-							type='text'
-						></input>
-					</div>
-					<div className='flex flex-col'>
-						<label className='text-violet text-xs font-bold'>Prénom</label>
-						<input
-							className='bg-violet border border-gray-400 rounded-md p-1 focus:outline-none focus:border-blue-500'
-							type='text'
+							type='email'
+							onChange={(e) => handleInputChange(e, "email")}
 						></input>
 					</div>
 					<div className='flex flex-col'>
 						<label className='text-violet text-xs font-bold'>
-							Date de naissance
+							Mot de passe <span className='text-rouge'>*</span>
+						</label>
+						<input
+							className='bg-violet border border-gray-400 rounded-md p-1 focus:outline-none focus:border-blue-500'
+							type='password'
+							onChange={(e) => handleInputChange(e, "password")}
+						></input>
+					</div>
+					<div>
+						<input type='file' onChange={handleFileChange} />
+						{previewUrl && (
+							<img
+								src={previewUrl}
+								alt='Preview'
+								className='w-20 h-20 rounded-full'
+							/>
+						)}
+					</div>
+				</div>
+
+				<div className='grid grid-cols-3 gap-8 mx-4 mb-10'>
+					<div className='flex flex-col'>
+						<label className='text-violet text-xs font-bold'>
+							Nom <span className='text-rouge'>*</span>
+						</label>
+						<input
+							className='bg-violet border border-gray-400 rounded-md p-1 focus:outline-none focus:border-blue-500'
+							type='text'
+							onChange={(e) => handleInputChange(e, "nom")}
+						></input>
+					</div>
+					<div className='flex flex-col'>
+						<label className='text-violet text-xs font-bold'>
+							Prénom <span className='text-rouge'>*</span>
+						</label>
+						<input
+							className='bg-violet border border-gray-400 rounded-md p-1 focus:outline-none focus:border-blue-500'
+							type='text'
+							onChange={(e) => handleInputChange(e, "prenom")}
+						></input>
+					</div>
+					<div className='flex flex-col'>
+						<label className='text-violet text-xs font-bold'>
+							Date de naissance <span className='text-rouge'>*</span>
 						</label>
 						<input
 							className='bg-violet border border-gray-400 rounded-md p-1 text-sm focus:outline-none focus:border-blue-500'
 							type='date'
-						></input>
-					</div>
-					<div className='flex flex-col'>
-						<label className='text-violet text-xs font-bold'>Nationalité</label>
-						<input
-							className='bg-violet border border-gray-400 rounded-md p-1 focus:outline-none focus:border-blue-500'
-							type='password'
+							onChange={(e) => handleInputChange(e, "date_naissance")}
 						></input>
 					</div>
 				</div>
 
-				<div className='grid grid-cols-4 gap-8 mx-4 mb-6'>
+				<div className='grid grid-cols-3 gap-8 mx-4 mb-10'>
 					<div className='flex flex-col'>
 						<label className='text-violet text-xs font-bold'>
-							Numéro de téléphone
+							Nationalité <span className='text-rouge'>*</span>
 						</label>
 						<input
 							className='bg-violet border border-gray-400 rounded-md p-1 focus:outline-none focus:border-blue-500'
-							type='password'
-						></input>
-					</div>
-					<div className='flex flex-col'>
-						<label className='text-violet text-xs font-bold'>Email</label>
-						<input
-							className='bg-violet border border-gray-400 rounded-md p-1 focus:outline-none focus:border-blue-500'
-							type='password'
+							type='text'
+							onChange={(e) => handleInputChange(e, "nationalite")}
 						></input>
 					</div>
 					<div className='flex flex-col'>
@@ -65,6 +178,18 @@ export function InscriptionChercheur() {
 						<input
 							className='bg-violet border border-gray-400 rounded-md p-1 focus:outline-none focus:border-blue-500'
 							type='text'
+							onChange={(e) => handleInputChange(e, "ville")}
+						></input>
+					</div>
+
+					<div className='flex flex-col'>
+						<label className='text-violet text-xs font-bold'>
+							Numéro de téléphone
+						</label>
+						<input
+							className='bg-violet border border-gray-400 rounded-md p-1 focus:outline-none focus:border-blue-500'
+							type='tel'
+							onChange={(e) => handleInputChange(e, "numero")}
 						></input>
 					</div>
 				</div>
@@ -95,18 +220,6 @@ export function InscriptionChercheur() {
 					</div>
 				</div>
 
-				<div className='grid grid-cols-3 gap-8 mx-4 mb-10'>
-					<div className='flex flex-col col-span-2'>
-						<label className='text-violet text-xs font-bold'>
-							Commentaires
-						</label>
-						<textarea
-							className='bg-violet border border-gray-400 rounded-md p-1 focus:outline-none focus:border-blue-500'
-							rows='4'
-						></textarea>
-					</div>
-				</div>
-
 				<div className='flex justify-end mr-4'>
 					<ButtonRond
 						couleur={"rouge"}
@@ -114,6 +227,7 @@ export function InscriptionChercheur() {
 						contenu={"Continuer"}
 						width={"w-1/4"}
 						height={"fit"}
+						onClick={() => handleSubmit()}
 					></ButtonRond>
 				</div>
 			</div>
